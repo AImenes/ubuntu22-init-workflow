d3c1e04b28f7c8c1d9d7659bac54e7689fbb1b81
Anders Imenes <aimenes@icloud.com>
2024-03-22 16:04:03 +0100

    bash

---
+++ quick_start_2_packages.sh
@@ -0,0 +1,14 @@
+#bin/bash
+
+
+sudo apt install build-essential python3 python3-pip nodejs npm default-jdk zsh wget curl vim htop tree gparted git ufw vlc -y
+
+sudo apt install wireshark nmap -y
+
+sudo snap install discord --classic
+
+sudo snap install code --classic
+
+sudo snap install postman --classic
+
+
